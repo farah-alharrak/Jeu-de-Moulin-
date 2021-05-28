8f7fc76bc4a8c2eedddf8b9b27ad361e241f0a8b
farah-alharrak <79784335+farah-alharrak@users.noreply.github.com>
2021-05-28 21:42:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,35 @@
+/** propriétaires : Farah Al Harrak && Ayyoub Kerroumi
+  
+  * Date de fin : mai 2021
+  
+  **/
+
+/************************************************************************************
+ *                                  Expat License                                   *
+ *  ------------------------------------------------------------------------------  *
+ * Copyright 2021 Farah Al Harrak && Ayyoub Kerroumi                                *
+ *                                                                                  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy     *
+ * of this software and associated documentation files (the "Software"), to deal    *
+ * in the Software without restriction, including without limitation the rights     *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        *
+ * copies of the Software, and to permit persons to whom the Software is            *
+ * furnished to do so, subject to the following conditions:                         *
+ *                                                                                  *
+ * The above copyright notice and this permission notice shall be included in       *
+ * all copies or substantial portions of the Software.                              *
+ *                                                                                  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    *
+ * SOFTWARE.                                                                        *
+ *                                                                                  *
+ ************************************************************************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "focntions.h"
@@ -6,17 +38,11 @@
 
 int main()
 {
-/*
+
     bienvenue();
     init();
-	gestion();*/
-    int Jeu[24]={1,2,1,0,1,2,0,1,0,0,0,2,2,0,2,1,0,0,0,0,0,0,0,0};
-	int dep, arr, score;
-	int x = minimax2(Jeu,5,1,2,&dep,&arr, &score);
-    printf("le nombre de noeuds explores en MINIMAX est: %d\n",x);
-
-    int y = alphabeta2(Jeu,5,1,2,&dep,&arr,-MAX,+MAX,&score);
-    printf("le nombre de noeuds explores en ALPHABETA est: %d\n",y);
+    gestion();
+    
 
     return 0;
 }
